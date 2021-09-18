2c29bc7f9901830ad68d7847a983f8cc9346d756
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-09-19 02:42:39 +0600

    add methods

---
+++ JAVA_Revision/src/JavaPractice/JavaMethods.java
@@ -0,0 +1,15 @@
+package JavaPractice;
+
+public class JavaMethods {
+
+	static void myMethod() {
+		System.out.println("This is a message from method created");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		myMethod();
+
+	}
+
+}
