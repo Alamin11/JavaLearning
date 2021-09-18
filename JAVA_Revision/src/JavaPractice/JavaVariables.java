788fce3f226f847924cff22eda9252c699779ad4
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-09-18 22:15:17 +0600

    Add variables

---
+++ JAVA_Revision/src/JavaPractice/JavaVariables.java
@@ -0,0 +1,18 @@
+package JavaPractice;
+
+public class JavaVariables {
+	
+	
+	
+	public static void main(String []args) {
+		String name = "Al amin";
+		int age = 28;
+		final int salary = 25000;
+		char ch = 'A';
+		double pi = 3.1416;
+		System.out.print("My name is " + name + ". My age is about " + age + ".\nI want to work as a JAVA developer.\nMy expected salary is around " + salary + "K.\n");
+		System.out.println("My favorite character is " + ch + " and my favorite number is "+ pi);
+		
+	}
+
+}
