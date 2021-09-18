d86fa8336501b55f6d0038a7e40d2f01fc19807c
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-09-18 21:43:04 +0600

    Add java revision project

---
+++ JAVA_Revision/src/JavaPractice/MainCls.java
@@ -0,0 +1,11 @@
+package JavaPractice;
+
+public class MainCls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello again java");
+
+	}
+
+}
